1b5a38055b39411f3d3a00b9fcf130164d902988
10 <10@BKS10032020>
2021-12-03 23:43:09 +0700

    Update MenuItem and ChangeCustomer

--- src/main/java/com/example/demo/ForgotPassword.java
+++ src/main/java/com/example/demo/ForgotPassword.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import javax.swing.*;
 import java.io.IOException;
 import java.sql.Connection;
